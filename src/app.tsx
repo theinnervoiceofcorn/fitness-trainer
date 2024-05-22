521bb3515a691da3c9972a92b6db73df98747dc7
theinnervoiceofcorn <kudrat.kenzhaev.official@gmail.com>
2024-05-22 14:53:47 +0500

    button onHover fix

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { MainPage, Offer, PrivacyPolicy, ProgramPage } from "./pages";
 
 export const App = () => {
+  document.addEventListener("touchstart", function () {
+    document.body.classList.add("no-hover");
+  });
+  document.addEventListener("mousemove", function () {
+    document.body.classList.remove("no-hover");
+  });
+
   const router = createBrowserRouter([
     {
       path: "/",
