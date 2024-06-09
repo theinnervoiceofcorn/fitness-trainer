714c7f8c44ba62a67edb24d0dc0fb4fb744f12f3
theinnervoiceofcorn <kudrat.kenzhaev.official@gmail.com>
2024-06-09 13:44:32 +0500

    coco loco

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
-import { Navbar } from "./01-navbar/navbar";
-import { YoutubeVideo } from "./02-yt-video/yt-video";
+import { YoutubeVideo } from "./02-youtube-video/yt-video";
 import { ProgramCard } from "./03-program-card/program-card";
 import { ProgramsFilter } from "./04-programs-filter/ui/programs-filter";
 import { Footer } from "./05-footer/footer";
 
-export { Navbar, YoutubeVideo, ProgramCard, ProgramsFilter, Footer };
+export { YoutubeVideo, ProgramCard, ProgramsFilter, Footer };
